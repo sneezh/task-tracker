a81258deea100993c168dac3735b1c5d8f415017
Vladislav Abrashkin <isneezh@gmail.com>
2021-04-03 18:15:24 +0300

    init symfony

---
+++ Makefile
@@ -0,0 +1,35 @@
+dc := docker-compose
+de := docker-compose exec
+de-php := $(de) php bash -c
+
+reup: down up
+
+rebuild: down build up
+
+build:
+	$(dc) build php
+
+bash:
+	$(de) php '/bin/bash'
+
+nginx-sh:
+	$(dc) exec nginx sh
+
+up:
+	$(dc) up -d
+
+down:
+	$(dc) down
+
+composer:
+	$(de-php) '$(MAKECMDGOALS)'
+
+composer-install:
+	$(de-php) 'composer install'
+
+
+console:
+	$(de-php) "bin/console $(filter-out $@,$(MAKECMDGOALS))"
+
+migrate:
+	$(de-php) 'bin/console doctrine:migrations:migrate --no-interaction'
---
+++ src/Controller/AppInfo.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class AppInfo
+{
+    /**
+     * @Route("/api/info", methods={"GET"})
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns the app info",
+     *     @SWG\Schema(type="object",
+     *         @SWG\Property(property="Name",type="string"),
+     *         @SWG\Property(property="Version",type="string"),
+     *         @SWG\Property(property="Author",type="string")
+     *     )
+     * )
+     * @SWG\Tag(name="Info")
+     */
+    public function index(): Response
+    {
+        $appInfo = [
+            'Name' => 'Task Tracker',
+            'Version' => '0.0.1',
+            'Author' => 'sneezh'
+        ];
+        return new Response(json_encode($appInfo, JSON_THROW_ON_ERROR));
+    }
+}
